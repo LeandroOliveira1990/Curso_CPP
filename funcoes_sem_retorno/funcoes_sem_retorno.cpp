7a6b36c79df238a654fe0ad019f3fdb349eaaf85
Leandro de Oliveira Silva <110729164+LeandroOliveira1990@users.noreply.github.com>
2023-09-22 15:37:13 -0300

    Aula sobre funções sem retorno VOID

---
+++ funcoes_sem_retorno/funcoes_sem_retorno.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//função que desenha borda
+void desenhaBorda(){
+    printf("\n|----------*------*--------|\n");
+
+}
+
+int main(int argc, char const *argv[])
+{
+    //chamando função
+    desenhaBorda();
+    //Imprimindo texto na tela
+    printf("TESTE");
+    //chamando função
+    desenhaBorda();
+    return 0;
+}
